e652c95549ea0591f0fa5df31faf20a4c7041688
weitang114 <weitang114@weitang114.local>
2015-09-06 15:30:47 +0800

    Add "Close Tab" function

--- popup.js
+++ popup.js
@@ -6,6 +6,7 @@ var _tabs;
 function showTabs(tabs) {
   _tabs = tabs;
   var list = $('#tabs');
+  list.empty();
   for (var i = 0; i < tabs.length; i++) {
     var t = tabs[i];
     console.log(t.title);
@@ -29,16 +30,19 @@ function showTabs(tabs) {
       class: 'text'
     });
 
+    var badge = $('<span />', {
+      class: 'glyphicon glyphicon-remove-circle pull-right close-btn',
+    });
+    badge.css('margin-left', 10);
+    badge.css('font-size', 16);
+
     href.append(icon);
+    href.append(badge);
     href.append(div);
     list.append(href);
   }
 
-  $('html').height(list.height());
-  list.css('overflowY', 'auto');
-  list.height(Math.min(list.height(), 600));
-
-
+  updateLayout();
   highlightActiveTab();
 
   $('.item').click(function() {
@@ -47,6 +51,30 @@ function showTabs(tabs) {
     chrome.tabs.update(_tabs[idx].id, {active: true});
     window.close();
   });
+
+  $('.close-btn').click(function() {
+    // stop the event from triggering the '.item' click callback
+    event.stopPropagation();
+
+    var item = $(this).parent();
+    var idx = item.index();
+    chrome.tabs.remove(_tabs[idx].id, function() {
+      item.remove();
+      _tabs.splice(idx, 1);
+
+      updateLayout();
+    });
+
+  });
+}
+
+function updateLayout() {
+  var list = $('#tabs');
+
+  $('html').height(list.height());
+  console.log('new height:' + list.height());
+  list.css('overflowY', 'auto');
+  list.css('max-height', Math.min(list.height(), 600));
 }
 
 function highlightActiveTab() {
